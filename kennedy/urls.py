ddee86d8a74c4ed750384b4d5a75e340d9a670b7
Nick Catalano <nickcatal@gmail.com>
2018-03-27 18:44:43 +0000

    Add templates and static assets. Add robots.txt
    We'll need to handle the OFA logos being in the repo at some point.

--- kennedy/urls.py
+++ kennedy/urls.py
@@ -6,6 +6,10 @@ from django.views.generic import TemplateView, RedirectView
 
 # pylint: disable=invalid-name
 urlpatterns = [
+    # By default disable bots
+    url(r'^robots\.txt$', TemplateView.as_view(
+        template_name="robots.txt", content_type='text/plain')),
+
     url(r'^admin/', admin.site.urls),
     url(r'^404/$', TemplateView.as_view(template_name='404.html')),
 ]
