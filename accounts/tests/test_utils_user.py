46be6fc1fc77335132eb513d90ad2b283c051062
Nick Catalano <nickcatal@gmail.com>
2018-03-28 16:23:23 -0500

    Add tests to user creation. Cleanup templates

---
+++ accounts/tests/test_utils_user.py
@@ -0,0 +1,65 @@
+"""Tests for the utils_user.py file in Accounts"""
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+from model_mommy import mommy
+from mock import patch
+
+from accounts.models import User
+from accounts.utils_user import create_user
+from location.models import UserLocation
+from kennedy_common.utils.tests import KennedyTestMixin
+
+
+class TestCreateUser(KennedyTestMixin, TestCase):
+    """Test the create_user method"""
+    def setUp(self):
+        """Setup the test"""
+        self.organization = mommy.make('branding.Organization')
+
+    def test_invalid_email(self):
+        """Test sending in an invalid email"""
+        with self.assertRaises(ValidationError) as error:
+            create_user(
+                self.organization, 'fakeemail', '60657', 'First', 'Last')
+
+        self.assertEqual(error.exception.message, u'Invalid Email Address')
+
+    @patch('accounts.utils_user.get_location')
+    def test_existing_user(self, mock):
+        """Test a case where there is an existing user with the same email"""
+        mock_location = self.create_location()
+        mock.return_value = mock_location
+
+        user = create_user(self.organization, 'validemail@localhost.com',
+                           '60657', 'First', 'Last')
+
+        self.assertTrue(UserLocation.objects.filter(
+            user=user, location=mock_location, is_active=True).exists())
+
+        with self.assertRaises(ValidationError) as error:
+            create_user(self.organization, 'validemail@localhost.com',
+                        '20009', 'First', 'Last')
+
+        self.assertEqual(
+            error.exception.message,
+            u'User with that email address already exists')
+
+    @patch('accounts.utils_user.get_location')
+    def test_creates_user(self, mock):
+        """Test that a new user is created"""
+        mock_location = self.create_location()
+        mock.return_value = mock_location
+
+        user = create_user(self.organization, 'email@localhost.com',
+                           '60657', 'First', 'Last')
+
+        newuser = User.objects.get(pk=user.pk)
+
+        self.assertEqual(newuser.organization, self.organization)
+        self.assertEqual(newuser.email, 'email@localhost.com')
+        self.assertEqual(newuser.first_name, 'First')
+        self.assertEqual(newuser.last_name, 'Last')
+
+        # Verify creates UserLocation
+        self.assertTrue(UserLocation.objects.filter(
+            user=newuser, location=mock_location, is_active=True).exists())
---
+++ kennedy_common/utils/tests.py
@@ -0,0 +1,55 @@
+"""Common mixins for tests"""
+import os
+import json
+import uuid
+
+from accounts.utils_user import create_user
+from branding.models import Organization
+from location.utils import get_location
+
+# pylint: disable=invalid-name
+path = os.path.dirname(os.path.abspath(__file__))
+illinois_file = os.path.join(
+    path, 'data/geocodio_illinois.json')
+with open(illinois_file) as json_file:
+    illinois_geocode = json.loads(json_file.read())
+
+
+class KennedyTestMixin(object):
+    """Mixin for Kennedy tests"""
+
+    def create_location(self, state_id='IL', address=None):
+        """Create a new Location"""
+        from mock import patch, Mock
+        if not address:
+            address = unicode(uuid.uuid4())
+
+        with patch('location.utils.GeocodioClient') as patch:
+            mock_geocode_result = Mock()
+            mock_geocode_result.geocode.return_value = illinois_geocode
+            patch.return_value = mock_geocode_result
+
+            return get_location(address)
+
+    def create_user(self, organization=None, location=None):
+        """Create a new user"""
+        from mock import patch
+
+        # If no organization is passed in, use the default one created in the
+        # branding migration
+        if not organization:
+            organization = Organization.objects.get(pk=1)
+
+        with patch('accounts.utils_user.get_location') as patch:
+            patch.return_value = self.create_location()
+            email = unicode(uuid.uuid4()) + '@localhost.com'
+            return create_user(organization, email, '20009', 'Joe', 'Smith')
+
+    def create_superuser(self, organization=None):
+        """Create a new superuser"""
+        user = self.create_user(organization)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+
+        return user
