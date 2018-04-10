25670e9573f3f9a22c9a0bde89de08ed2eecb9a2
Nick Catalano <nickcatal@gmail.com>
2018-04-10 12:49:48 -0500

    Fix issue in branding middleware
    Because we were using the same variable name (`domain`) in both the
    cache key and as an object, we were accidentally passing that string to
    the middleware. This fixes that.
    
    Also, caching is broken on prod, because this should've showed up a long
    time ago and only appeared now because I was setting up the staging
    server which does have a valid cache running.

--- branding/middleware.py
+++ branding/middleware.py
@@ -19,8 +19,8 @@ class BrandingMiddleware(object):
     def __call__(self, request):
         """Process a request"""
 
-        domain = split_domain_port(request.get_host())[0]
-        cache_key = org_domain_cache_key(domain)
+        domain_name = split_domain_port(request.get_host())[0]
+        cache_key = org_domain_cache_key(domain_name)
 
         organization = cache.get(cache_key)
 
@@ -29,7 +29,7 @@ class BrandingMiddleware(object):
                 'organization', 'organization__primary_domain')
 
             try:
-                domain = domain_queryset.get(hostname=domain)
+                domain = domain_queryset.get(hostname=domain_name)
             except Domain.DoesNotExist:
                 if settings.DEBUG:
                     domain = domain_queryset.first()
@@ -41,6 +41,6 @@ class BrandingMiddleware(object):
             if not settings.DEBUG:
                 cache.set(cache_key, organization)
 
-        request.organization = domain.organization
+        request.organization = organization
 
         return self.get_response(request)
