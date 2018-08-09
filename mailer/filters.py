7f67d7c27d95d03c548a4f1a2ff5435907dafd84
Nick Catalano <nickcatal@gmail.com>
2018-08-08 21:02:09 -0500

    Allow filtering of templates by deadline and election type

--- mailer/filters.py
+++ mailer/filters.py
@@ -2,14 +2,19 @@
 import django_filters
 
 from mailer.models import MailingTemplate
+from election.choices import DEADLINES, ELECTION_TYPES
 
 
 class MailingTemplateFilter(django_filters.FilterSet):
     """Filter for the user list in the management panel"""
     name = django_filters.CharFilter(
         lookup_expr='icontains', label='Name Contains')
+    election_type = django_filters.ChoiceFilter(
+        label='Election Type', choices=ELECTION_TYPES)
+    deadline_type = django_filters.ChoiceFilter(
+        label='Deadline', choices=DEADLINES)
 
     class Meta(object):
         """Meta options for the filter"""
         model = MailingTemplate
-        fields = ['name']
+        fields = ['name', 'election_type', 'deadline_type']
